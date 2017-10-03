9cb33fd24a7244bfb1846ecc8e000eb0d0b57045
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-03 08:43:49 -0600

    added a donut class

---
+++ src/useful/model/Donut.java
@@ -0,0 +1,15 @@
+package useful.model;
+
+public class Donut 
+{
+	private boolean hasSprinkles;
+	private boolean isGlazed;
+	private String shape;
+	private String flavor;
+	private int holeCount;
+	
+	public Donut()
+	{
+		
+	}
+}
