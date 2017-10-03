a9e4e6b7bfe05a82361d2de9c875a7bd1da07538
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-03 08:56:44 -0600

    added comments and added to the toolController.

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -1,5 +1,25 @@
 package useful.controller;
+//Imports everything
+import java.util.List;
+import java.util.ArrayList;
+//imports useful.model
+import useful.model.Donut;
 
-public class ToolController {
-
+public class ToolController 
+{
+	private List<Donut> donutList;
+	
+	public ToolController()
+	{
+		//donutlist is a new array list
+		donutList = new ArrayList<Donut>();
+	}
+	
+	public void start()
+	{
+		//mes a new donut
+		Donut temp = new Donut();
+		
+		donutList.add(temp);
+	}
 }
