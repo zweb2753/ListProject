b8d6857f25fdea4ad1044a7fdf2bef0d55e6667f
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-03 08:31:34 -0600

    Initial commit

---
+++ src/useful/controller/ToolController.java
@@ -0,0 +1,5 @@
+package useful.controller;
+
+public class ToolController {
+
+}
---
+++ src/useful/controller/ToolRunner.java
@@ -0,0 +1,15 @@
+package useful.controller;
+
+/**
+ * 
+ * @author zweb2753
+ *
+ */
+public class ToolRunner 
+{
+	public static void main (String [] args)
+	{
+		ToolController myApp = new ToolController();
+		myApp.start();
+	}
+}
